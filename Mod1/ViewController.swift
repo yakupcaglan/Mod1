8ef5ea92412a3949971a25618b7dd182aa5c8fee
Yakup Çağlan <yakup.caglan@hepsiburada.com>
2022-07-07 13:07:56 +0300

    Initial Commit

---
+++ Mod1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mod1
+//
+//  Created by Yakup Çağlan on 7.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
